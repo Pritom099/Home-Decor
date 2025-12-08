1a28b48144fc30e09bbfbed073386bbff71a1719
Pritom099 <pritompaul515@gmail.com>
2025-12-08 15:01:49 +0600

    loading commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Link } from 'react-router';
 import ProductCard from '../Components/ProductCard';
 import useProducts from '../hooks/useProducts';
+import LoadingSpinner from '../Components/LoadingSpinner';
 
 const Home = () => {
-    
+
     // console.log(products)
-    const {products, loading, error} = useProducts()
+    const { products, loading, error } = useProducts()
     console.log(products)
     const featuredProducts = products.slice(0, 6);
     return (
@@ -15,13 +16,20 @@ const Home = () => {
                 <h1 className='text-3xl font-semibold'>Featured Products</h1>
                 <Link className='btn btn-outline' to='/products'>See All Products</Link>
             </div>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-                {
-                    featuredProducts.map(product => (
-                        <ProductCard key={product.id} product={product}></ProductCard>
-                    ))
-                }
-            </div>
+
+            {
+                loading ? (
+                    <LoadingSpinner></LoadingSpinner>
+                ) : (
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                        {
+                            featuredProducts.map(product => (
+                                <ProductCard key={product.id} product={product}></ProductCard>
+                            ))
+                        }
+                    </div>
+                )
+            }
         </div>
     );
 };
